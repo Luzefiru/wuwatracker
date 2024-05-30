061186fa5f46e54f88aa45bb55572a3c817df1ca
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-05-30 22:57:26 +0800

    style: make sidebar sticky

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@ import { ConveneCategories } from "../convenes/convene-categories";
 
 export function Sidebar() {
   return (
-    <aside className="w-full desktop:w-[45%] h-full ">
+    <aside className="w-full desktop:w-[45%] h-fit desktop:sticky desktop:top-16 ">
       <div className="ml-5 mr-5 desktop:mr-0 flex flex-col gap-3">
         <ConveneCategories />
       </div>
