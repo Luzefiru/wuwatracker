6a3e93309f13311fca5e8d5603a0a5b77b607c33
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-05-30 16:19:17 +0800

    feat: add cards in home page

--- src/components/convenes/convene-stats.tsx
+++ src/components/convenes/convene-stats.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import * as React from "react";
 import {
   Card,
@@ -6,21 +7,52 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import { CharacterLifetimeStats } from "./character-lifetime-stats";
+import { WeaponLifetimeStats } from "./weapon-lifetime-statistics";
 
 export function ConveneStats() {
   return (
-    <div className="flex gap-3 w-full">
-      <Card className="mx-2 mt-5 bg-accent w-[25%]">
+    <div className="flex gap-3 w-full flex-col">
+      <div className="flex w-full ">
+        <Card className="mx-2 mt-5 bg-accent w-[50%]">
+          <div className="flex">
+            <div className="w-full">
+              <CardHeader>
+                <CardTitle>Character Banner Statistics</CardTitle>
+                <CardDescription>Account Lifetime Statistics</CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-col mt-3 gap-2">
+                <CharacterLifetimeStats />
+              </CardContent>
+            </div>
+          </div>
+        </Card>
+        <Card className="mx-2 mt-5 bg-accent w-[50%]">
+          <div className="flex">
+            <div className="w-full">
+              <CardHeader>
+                <CardTitle>Weapon Banner Statistics</CardTitle>
+                <CardDescription>Account Lifetime Statistics</CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-col mt-3 gap-2">
+                <WeaponLifetimeStats />
+              </CardContent>
+            </div>
+          </div>
+        </Card>
+      </div>
+      <Card className="mx-2 mt-5  bg-accent w-full">
         <div className="flex">
           <div className="w-full">
             <CardHeader>
-              <CardTitle>Lifetime Statistics</CardTitle>
-              <CardDescription>Prevail the Lasting Night</CardDescription>
+              <CardTitle>Recent 5 Stars Received</CardTitle>
             </CardHeader>
-            <CardContent className="flex flex-col mt-1">
-              <Card className="py-5 px-2">
-                <h1>Astirites Spent</h1>
-              </Card>
+            <CardContent className="flex mt-5 mb-3 gap-2 ">
+              <div className="bg-blue-300 w-12 h-12 rounded-full"></div>
+              <div className="bg-blue-300 w-12 h-12 rounded-full"></div>
+              <div className="bg-blue-300 w-12 h-12 rounded-full"></div>
+              <div className="bg-blue-300 w-12 h-12 rounded-full"></div>
+              <div className="bg-blue-300 w-12 h-12 rounded-full"></div>
             </CardContent>
           </div>
         </div>
---
+++ src/components/convenes/weapon-lifetime-statistics.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import * as React from "react";
+
+export function WeaponLifetimeStats() {
+  return (
+    <>
+      <div className="py-4 px-3 flex justify-between bg-background rounded-lg">
+        <h1 className="text-lg">Astirites Spent</h1>
+        <div className="flex gap-1">
+          <h1 className="text-lg">20000</h1>
+          <Image
+            src={"/icons/astirites.png"}
+            width={30}
+            height={8}
+            alt="astirites icon"
+          />
+        </div>
+      </div>
+      <div className="py-4 px-3 flex justify-between bg-background rounded-lg">
+        <h1 className="text-lg">Total Convenes</h1>
+        <div className="flex gap-1">
+          <h1 className="text-lg">200</h1>
+          <Image
+            src={"/icons/special-convene.png"}
+            width={30}
+            height={8}
+            alt="astirites icon"
+          />
+        </div>
+      </div>
+      <div className="py-4 px-3 flex justify-between bg-background rounded-lg">
+        <h1 className="text-lg">5 Star Characters</h1>
+        <div className="flex">
+          <h1 className="text-lg">5</h1>
+        </div>
+      </div>
+      <div className="py-4 px-3 flex justify-between bg-background rounded-lg">
+        <h1 className="text-lg">Average Pity</h1>
+        <div className="flex">
+          <h1 className="text-lg">56</h1>
+        </div>
+      </div>
+    </>
+  );
+}
