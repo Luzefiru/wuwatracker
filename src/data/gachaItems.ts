baaefcced41c92f2ae78aeeb47cccbb1fcc3602f
Luzefiru <ian.deejay.iv@gmail.com>
2024-05-31 17:00:13 +0800

    feat: add gachaItems data

---
+++ src/data/gachaItems.ts
@@ -0,0 +1,75 @@
+export const fiveStarCharacterMetadata = {
+  Jiyan: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_jiyan_UI.png',
+  },
+  Yinlin: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_yinlin_UI.png',
+  },
+  Calcharo: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_kakaluo_UI.png',
+  },
+  Verina: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_jueyuan_UI.png',
+  },
+  Jianxin: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_jianxin_UI.png',
+  },
+  Lingyang: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_lingyang_UI.png',
+  },
+  Encore: {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw_anke_UI.png',
+  },
+} as const;
+
+export const fiveStarWeaponMetadata = {
+  'Verdant Summit': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21010016_UI.png',
+  },
+  'Abyss Surges': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21040015_UI.png',
+  },
+  'Cosmic Ripples': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21050015_UI.png',
+  },
+  'Emerald of Genesis': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21020015_UI.png',
+  },
+  'Lustrous Razor': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21010015_UI.png',
+  },
+  'Static Mist': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21030015_UI.png',
+  },
+  'String Master': {
+    qualityLevel: 5,
+    imgSrc:
+      'https://cdn.wanderer.moe/wuthering-waves/luckdraw/T_Luckdraw21050016_UI.png',
+  },
+} as const;
