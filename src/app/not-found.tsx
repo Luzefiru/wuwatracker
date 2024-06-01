22240740a25e1547b7ff710f33e5ea6dabc77ef8
Luzefiru <ian.deejay.iv@gmail.com>
2024-06-01 18:03:38 +0800

    style: refactor 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,32 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Home } from 'lucide-react';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-5 items-center justify-center h-full px-8 desktop:px-0">
-      <p className="text-2xl desktop:text-3xl font-bold">
-        Whoops! Are you lost, Rover?
-      </p>
-      <Image
-        className="object-cover"
-        src="https://cdn.wanderer.moe/wuthering-waves/emotes/T_ChatEmo_A_06.png"
-        width="360"
-        height="450"
-        alt="Wuwa Convene Tracker"
-      />
-      <div className="flex flex-col gap-4">
-        <p className="text-md desktop:text-lg">
-          Sorry, we could not seem to find that resource.
+    <div className="flex justify-center h-[80vh] lg:h-auto lg:py-12">
+      <section className="flex flex-col items-center justify-center h-full px-8 desktop:px-0 max-w-screen-lg text-center">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
+          Whoops! <br />
+          Are you lost, <span className="text-yellow-500">Rover</span>?
+        </h1>
+        <Image
+          className="object-cover mask-gradient-to-b"
+          src="https://cdn.wanderer.moe/wuthering-waves/emotes/T_ChatEmo_A_06.png"
+          width="360"
+          height="450"
+          alt="404: Page Not Found"
+        />
+        <p className="leading-7 text-sm sm:text-lg text-muted-foreground mb-8">
+          The page you&apos;re looking for doesn&apos;t exist.
         </p>
         <Button asChild variant="default" size="lg">
-          <Link href="/">Return to Home</Link>
+          <Link href="/">
+            <Home className="w-4 h-4 mr-2" /> Return to Home
+          </Link>
         </Button>
-      </div>
+      </section>
     </div>
   );
 }
