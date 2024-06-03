2e31da82ee35c18b524bf16beefceb5d9f912e0f
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-06-03 20:41:42 +0800

    feat: add date to table

--- src/components/convenes/pull-history.tsx
+++ src/components/convenes/pull-history.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React from "react";
 import InfiniteScroll from "../ui/scroll-area";
-import { Loader2 } from "lucide-react";
+import getPullNumber from "@/lib/getPullNumber";
+
 import { useState } from "react";
 import { BannerStats } from "@/types/BannerStats";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -15,62 +16,11 @@ import {
 } from "@/components/ui/table";
 import { TableItemName } from "../ui/table-item-name";
 
-interface DummyProductResponse {
-  products: DummyProduct[];
-  total: number;
-  skip: number;
-  limit: number;
-}
-
-interface DummyProduct {
-  id: number;
-  title: string;
-  price: string;
-}
-
 interface Props {
   stats: BannerStats | null;
 }
 
-const Product = ({ product }: { product: DummyProduct }) => {
-  return (
-    <div className="flex w-full flex-col gap-2 rounded-lg border-2 border-gray-200 p-2">
-      <div className="flex gap-2">
-        <div className="flex flex-col justify-center gap-1">
-          <div className="font-bold text-primary">
-            {product.id} - {product.title}
-          </div>
-          <div className="text-sm text-muted-foreground">{product.price}</div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export function PullHistory({ stats }: Props) {
-  const [page, setPage] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
-  const [products, setProducts] = useState<DummyProduct[]>([]);
-
-  const next = async () => {
-    setLoading(true);
-
-    setTimeout(async () => {
-      const res = await fetch(
-        `https://dummyjson.com/products?limit=3&skip=${3 * page}&select=title,price`,
-      );
-      const data = (await res.json()) as DummyProductResponse;
-      setProducts((prev) => [...prev, ...data.products]);
-      setPage((prev) => prev + 1);
-
-      // Usually your response will tell you if there is no more data.
-      if (data.products.length < 3) {
-        setHasMore(false);
-      }
-      setLoading(false);
-    }, 800);
-  };
   return (
     <div className="max-h-[300px] w-full  overflow-y-auto">
       <div className="flex w-full flex-col items-center  gap-3">
@@ -101,21 +51,24 @@ export function PullHistory({ stats }: Props) {
                         qualityLevel={item.qualityLevel}
                       />
                     </TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
+                    <TableCell>
+                      {getPullNumber(
+                        item.qualityLevel,
+                        item.fourStarCurrentPity,
+                        item.pullNumber,
+                        item.previousFiveStarPullNumber,
+                      )}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {item.time.toLocaleString()}
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </CardContent>
         </Card>
-
-        <InfiniteScroll
-          hasMore={hasMore}
-          isLoading={loading}
-          next={next}
-          threshold={1}
-        ></InfiniteScroll>
+        <InfiniteScroll threshold={1}></InfiniteScroll>
       </div>
     </div>
   );
--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 
 interface InfiniteScrollProps {
-  isLoading: boolean;
-  hasMore: boolean;
-  next: () => unknown;
   threshold?: number;
   root?: Element | Document | null;
   rootMargin?: string;
@@ -12,9 +9,6 @@ interface InfiniteScrollProps {
 }
 
 export default function InfiniteScroll({
-  isLoading,
-  hasMore,
-  next,
   threshold = 1,
   root = null,
   rootMargin = "0px",
@@ -22,34 +16,6 @@ export default function InfiniteScroll({
   children,
 }: InfiniteScrollProps) {
   const observer = React.useRef<IntersectionObserver>();
-
-  const observerRef = React.useCallback(
-    (element: HTMLElement | null) => {
-      let safeThreshold = threshold;
-      if (threshold < 0 || threshold > 1) {
-        console.warn(
-          "threshold should be between 0 and 1. You are exceed the range. will use default value: 1",
-        );
-        safeThreshold = 1;
-      }
-
-      if (isLoading) return;
-
-      if (observer.current) observer.current.disconnect();
-      if (!element) return;
-
-      observer.current = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting && hasMore) {
-            next();
-          }
-        },
-        { threshold: safeThreshold, root, rootMargin },
-      );
-      observer.current.observe(element);
-    },
-    [hasMore, isLoading, next, threshold, root, rootMargin],
-  );
 
   const flattenChildren = React.useMemo(
     () => React.Children.toArray(children),
@@ -68,7 +34,7 @@ export default function InfiniteScroll({
         const isObserveTarget = reverse
           ? index === 0
           : index === flattenChildren.length - 1;
-        const ref = isObserveTarget ? observerRef : null;
+
         // @ts-ignore ignore ref type
         return React.cloneElement(child, { ref });
       })}
