dca7c32d2dec8d068d500a3d46907af08999e346
Luzefiru <ian.deejay.iv@gmail.com>
2024-06-05 19:45:11 +0800

    test: redirect after OAuth flow

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -23,11 +23,25 @@ export default function useAuth() {
   };
 
   const signInWithGoogle = async () => {
+    /**
+     * Helper function for auth `redirectTo`.
+     * See more: https://supabase.com/docs/guides/auth/redirect-urls
+     */
+    const getURL = () => {
+      let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        "http://localhost:3000/";
+      // Make sure to include `https://` when not localhost.
+      url = url.includes("http") ? url : `https://${url}`;
+      // Make sure to include a trailing `/`.
+      url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+      return url;
+    };
+
     await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: {
-        redirectTo: "/auth/callback",
-      },
+      options: { redirectTo: getURL() },
     });
   };
 
