ec1f496a16cfcfb6de3e8d90db02d542ea8b0493
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-06-07 12:08:10 +0800

    feat: create sequences route

--- src/app/(main-layout)/sequences/layout.tsx
+++ src/app/(main-layout)/sequences/layout.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Wuwa Tracker | Import",
+  title: "Wuwa Tracker | Sequences",
   description:
-    "Follow our interactive tutorial to display your Wuthering Waves data and view your statistics. Wuwa Tracker pity counter for Wuthering Waves, using the up-to-date data with global statistics and more. Share your pulls with your friends and track your account!",
+    "Check your latest Wuthering Waves pull statistics! Wuwa Tracker pity counter for Wuthering Waves, using the up-to-date data with global statistics and more. Share your pulls with your friends and track your account!",
 };
 
 export default function ImportLayout({
