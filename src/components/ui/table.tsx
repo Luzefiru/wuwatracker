fd2f2fd636256705e6e6a15a09b9d20e68f00d55
Luzefiru <ian.deejay.iv@gmail.com>
2024-06-09 14:59:11 +0800

    style: resize pull history table

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -87,7 +87,10 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn(
+      "py-2 px-4 align-middle [&:has([role=checkbox])]:pr-0",
+      className,
+    )}
     {...props}
   />
 ));
