6015ce909cf2d67a90ebbe7841a17effdee0f6b0
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-06-10 13:31:12 +0800

    nit: fix z index of item name

--- src/components/sequences/item-avatar.tsx
+++ src/components/sequences/item-avatar.tsx
@@ -25,7 +25,7 @@ export function ItemAvatar({ name, qualityLevel, numOfSequences }: Props) {
 
   return (
     <Card
-      className={`flex justify-between flex-col mb-1 border-none dark:bg-background/80 relative hover:bg-accent/40 transition-all duration-75`}
+      className={`flex justify-between flex-col mb-1 border-none bg-background/80 relative hover:bg-accent/40 transition-all duration-75`}
     >
       <CardContent className="py-2 h-fit relative px-0 flex items-center justify-center pb-0">
         <div className="w-24 place-self-center h-fit">
@@ -40,11 +40,12 @@ export function ItemAvatar({ name, qualityLevel, numOfSequences }: Props) {
           </Badge>
         )}
       </CardContent>
-      <CardFooter className="pb-2 h-15 w-full flex flex-col items-center justify-center z-100 ">
+      <CardFooter className="pb-2 h-15 w-full flex flex-col items-center justify-center">
         <p className="text-lg text-yellow-500">
           {qualityLevel === 5 ? "✦✦✦✦✦" : "✦✦✦✦"}
         </p>
-        <p className="w-20 text-center truncate">{name}</p>
+        <p className="w-20 text-center truncate z-20">{name}</p>
+
         <div
           className={`rounded-lg absolute inset-x-0 bottom-0 h-[90%] bg-gradient-to-t  via-transparent to-transparent z-10 ${qualityLevel === 5 ? "from-yellow-400/30" : "from-purple-400/30"}`}
         ></div>
