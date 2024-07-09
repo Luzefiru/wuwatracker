92c205094ba36b8f8c386725415288a94a7df8b8
Luzefiru <ian.deejay.iv@gmail.com>
2024-07-09 21:44:54 +0800

    feat: expose import script

---
+++ scripts.ts
@@ -0,0 +1,2 @@
+export const AUTO_IMPORT_SCRIPT_GIST_URL =
+  'https://gist.githubusercontent.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c/raw/529a4a53a265e0ee15f48e086654d66529cdb157/get-url.ps1';
