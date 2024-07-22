fe7debe2745ca5c281ad6b6d8300a92206efd899
Luzefiru <ian.deejay.iv@gmail.com>
2024-07-22 12:06:30 +0800

    feat: use theREalpha's auto import script

--- scripts.ts
+++ scripts.ts
@@ -1,2 +1,2 @@
 export const AUTO_IMPORT_SCRIPT_GIST_URL =
-  'https://gist.githubusercontent.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c/raw/529a4a53a265e0ee15f48e086654d66529cdb157/get-url.ps1';
+  'https://gist.githubusercontent.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c/raw/ad09333bf7186fe61afa873fb1534425869c31c3/get-url.ps1';
