ff9135ccaab2b82dae18c60de9b377670311ec22
Luzefiru <ian.deejay.iv@gmail.com>
2024-09-15 23:38:19 +0800

    feat(items): add resonator exp items

---
+++ items/exp-items.ts
@@ -0,0 +1,11 @@
+export interface ExpItem {
+  id: number;
+  exp: number;
+}
+
+export const RESONATOR_EXP_ITEMS: ExpItem[] = [
+  { id: 43010004, exp: 20000 }, // Premium Resonance Potion
+  { id: 43010003, exp: 8000 }, // Advanced Resonance Potion
+  { id: 43010002, exp: 3000 }, // Medium Resonance Potion
+  { id: 43010001, exp: 1000 }, // Basic Resonance Potion
+];
