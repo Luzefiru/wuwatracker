6af06e1e1db6c36c9b7c5cbae43529909f55b791
Luzefiru <ian.deejay.iv@gmail.com>
2024-09-21 16:52:04 +0800

    feat(items): add weapon exp materials

--- items/exp-items.ts
+++ items/exp-items.ts
@@ -9,3 +9,10 @@ export const RESONATOR_EXP_ITEMS: ExpItem[] = [
   { id: 43010002, exp: 3000 }, // Medium Resonance Potion
   { id: 43010001, exp: 1000 }, // Basic Resonance Potion
 ];
+
+export const WEAPON_EXP_ITEMS: ExpItem[] = [
+  { id: 43020004, exp: 20000 }, // Premium Energy Core
+  { id: 43020003, exp: 8000 }, // Advanced Energy Core
+  { id: 43020002, exp: 3000 }, // Medium Energy Core
+  { id: 43020001, exp: 1000 }, // Basic Energy Core
+];
