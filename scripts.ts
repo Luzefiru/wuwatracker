6becfbe7480e9470b7ec7510ae318ba5971c1dd1
Luzefiru <ian.deejay.iv@gmail.com>
2024-11-30 17:26:05 +0800

    docs(script): add GPLv3 license

--- scripts.ts
+++ scripts.ts
@@ -1,2 +1,2 @@
 export const AUTO_IMPORT_SCRIPT_GIST_URL =
-  "https://gist.githubusercontent.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c/raw/d776773fb71622218d522bf9b861c47394980d25/get-url.ps1";
+  "https://gist.githubusercontent.com/Luzefiru/19c0759bea1b9e7ef480bb39303b3f6c/raw/73538b27999212d7063ecf483a2f79dae96007d2/get-url.ps1";
