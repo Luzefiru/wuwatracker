fe7bb3a8fa276821c3201e422b37da5e48ac0223
Luzefiru <ian.deejay.iv@gmail.com>
2024-12-08 10:30:20 +0800

    feat(events): add ToA

--- events.ts
+++ events.ts
@@ -206,6 +206,15 @@ const rawEvents: { banners: Event[]; activities: Event[] } = {
       group: EVENT_GROUP.TOWER_OF_ADVERSITY,
     },
     {
+      name: "Tower of Adversity: Hazard Revisited",
+      img: "https://snipboard.io/NnA3YO.jpg",
+      startDate: "2024-12-09 04:00",
+      endDate: "2024-12-23 04:00",
+      color: "#18235f",
+      reliability: "Official",
+      group: EVENT_GROUP.TOWER_OF_ADVERSITY,
+    },
+    {
       name: "Pioneer Podcast",
       img: "https://snipboard.io/2Z7Ncr.jpg",
       description: `You can complete tasks in Pioneer Podcast and accumulate Podcast EXP. Level up your Podcast to get rich rewards. Unlock "Insider Channel" or "Connoisseur Channel" and level up your Podcast to win extra rewards including Astrite*680, a 4-star Golden Eternal Series Weapon of Choice, Radiant Tide, and more! Unlocking "Connoisseur Channel" grants an instant advance of Podcast level by 10, with the period-limited Sigil "Sound of Tides", Lustrous Tide*3, and Gilded Ginkgo (can be used to improve the Syntonization Rank of a 4-star Golden Eternal Series Weapon).`,
